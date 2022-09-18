01c37028fe24735df2d6a6e704926480ababa1a6
sallieeky <sallieeky@gmail.com>
2022-09-18 15:36:57 +0800

    setup env and api

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Str;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Str::random(60);
 });

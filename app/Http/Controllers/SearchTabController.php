e6495946597ddf65af200ca2517c2dc186e6af4b
sallieeky <sallieeky@gmail.com>
2022-10-02 07:36:14 +0800

    add api data history

---
+++ app/Http/Controllers/SearchTabController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Postingan;
+use Illuminate\Http\Request;
+
+class SearchTabController extends Controller
+{
+    public function getDataHistory(Request $request)
+    {
+        if ($request->header("API_KEY") != env("API_KEY")) {
+            return response()->json([
+                "status" => 403,
+                "message" => "Access denied"
+            ], 403);
+        }
+        $item_id = json_decode($request->id);
+        $data = Postingan::whereIn('item_id', $item_id)
+            ->with("item", "item.lokasi")
+            ->get();
+        return response()->json($data);
+    }
+}
